a9fc7e4297f5533856062c087e22a8eb3f8adf1f
Antony K Sebastian <antonyksebastian@antonys-air.home>
2025-08-11 19:29:02 +0100

    Using env variables

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,9 @@
 import { AppState, Platform } from 'react-native'
-import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient, processLock } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.YOUR_REACT_NATIVE_SUPABASE_URL
-const supabaseAnonKey = process.env.YOUR_REACT_NATIVE_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
