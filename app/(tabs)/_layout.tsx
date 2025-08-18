4c8e9966682a031600870e1effda553de164dfa8
Antony K Sebastian <antonyksebastian@Antonys-MacBook-Air.local>
2025-08-18 13:43:36 +0100

    Creating tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,64 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        headerStyle: { backgroundColor: "#f5f5f5" },
+        headerShadowVisible: false,
+        tabBarStyle: {
+          backgroundColor: "#f5f5f5",
+          borderTopWidth: 0,
+          elevation: 0,
+          shadowOpacity: 0,
+        },
+        tabBarActiveTintColor: "#6200ee",
+        tabBarInactiveTintColor: "#666666",
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Welcome",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="home-outline"
+              size={size}
+              color={color}
+
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Actions"
+        options={{
+          title: "Actions",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="check-circle-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="account-circle-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
